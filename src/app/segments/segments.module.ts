7ddc6b58384640fac1cb5d0b88a55b646abba038
Tim Acker <tjacker31@gmail.com>
2023-04-23 14:23:15 -0400

    stubs out modules with routing for project

---
+++ src/app/segments/segments.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SegmentsRoutingModule } from './segments-routing.module';
+
+@NgModule({
+	declarations: [],
+	imports: [CommonModule, SegmentsRoutingModule],
+})
+export class SegmentsModule {}

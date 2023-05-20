eb30fab5a3f8096bb68131126e59600421ec537f
Tim Acker <tjacker31@gmail.com>
2023-05-20 17:26:07 -0400

    creates tabs menu component in collections module

--- src/app/collections/collections-routing.module.ts
+++ src/app/collections/collections-routing.module.ts
@@ -1,9 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BiographyComponent } from './biography/biography.component';
 import { CollectionsSectionComponent } from './collections-section/collections-section.component';
+import { CompaniesComponent } from './companies/companies.component';
+import { PartnersComponent } from './partners/partners.component';
 
-const routes: Routes = [{ path: '', component: CollectionsSectionComponent }];
-
+const routes: Routes = [
+	{
+		path: '',
+		component: CollectionsSectionComponent,
+		children: [
+			{ path: '', component: BiographyComponent },
+			{ path: 'companies', component: CompaniesComponent },
+			{ path: 'partners', component: PartnersComponent },
+		],
+	},
+];
 @NgModule({
 	imports: [RouterModule.forChild(routes)],
 	exports: [RouterModule],
--- src/app/collections/collections.module.ts
+++ src/app/collections/collections.module.ts
@@ -1,12 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
+import { BiographyComponent } from './biography/biography.component';
 import { CollectionsRoutingModule } from './collections-routing.module';
 import { CollectionsSectionComponent } from './collections-section/collections-section.component';
+import { CompaniesComponent } from './companies/companies.component';
+import { PartnersComponent } from './partners/partners.component';
 import { TableComponent } from './table/table.component';
+import { TabsComponent } from './tabs/tabs.component';
 
 @NgModule({
-	declarations: [CollectionsSectionComponent, TableComponent],
+	declarations: [
+		CollectionsSectionComponent,
+		TableComponent,
+		BiographyComponent,
+		CompaniesComponent,
+		PartnersComponent,
+		TabsComponent,
+	],
 	imports: [CommonModule, CollectionsRoutingModule, SharedModule],
 	exports: [],
 })

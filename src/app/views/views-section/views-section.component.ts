65139bde2c48ea8d51ad143bbd4940328c9571e8
Tim Acker <tjacker31@gmail.com>
2023-05-20 17:45:38 -0400

    creates statistics component in views module

--- src/app/views/views-section/views-section.component.ts
+++ src/app/views/views-section/views-section.component.ts
@@ -5,4 +5,10 @@ import { Component } from '@angular/core';
 	templateUrl: './views-section.component.html',
 	styleUrls: ['./views-section.component.scss'],
 })
-export class ViewsSectionComponent {}
+export class ViewsSectionComponent {
+	public stats = [
+		{ value: 22, label: 'users' },
+		{ value: 9000, label: 'revenue', currency: true },
+		{ value: 50, label: 'reviews' },
+	];
+}
--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { SharedModule } from '../shared/shared.module';
+import { StatisticsComponent } from './statistics/statistics.component';
 import { ViewsRoutingModule } from './views-routing.module';
 import { ViewsSectionComponent } from './views-section/views-section.component';
 
 @NgModule({
-	declarations: [ViewsSectionComponent],
-	imports: [CommonModule, ViewsRoutingModule],
+	declarations: [ViewsSectionComponent, StatisticsComponent],
+	imports: [CommonModule, ViewsRoutingModule, SharedModule],
 	exports: [],
 })
 export class ViewsModule {}
